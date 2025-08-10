b36b4c6c018f99873c8bdd8e1a2ee9e27e286cdf
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-08-10 19:22:19 +0100

    modif

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './Components/App/App'
+import App from './components/App/App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient();
